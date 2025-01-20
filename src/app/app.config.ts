93de2b9a197634835fbdd9491a10c98869959c1c
saratrasv <cyrondelahoz@gmail.com>
2025-01-20 11:05:36 -0400

    refactor: update configuration and improve code formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,8 @@
-import {
-  type ApplicationConfig,
-  provideZoneChangeDetection,
-} from '@angular/core';
+import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
